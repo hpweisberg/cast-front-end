c166a34168f8c87cfcb3419c8ac36918074fe8ed
branhickman <branhickman@gmail.com>
2023-02-03 15:36:36 -0700

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import ChangePassword from './pages/ChangePassword/ChangePassword'
+import FakeComponent from './components/FakeComponent/FakeComponent'
 
 // components
 import NavBar from './components/NavBar/NavBar'
@@ -36,6 +37,7 @@ const App = () => {
   return (
     <>
       <NavBar user={user} handleLogout={handleLogout} />
+      <FakeComponent />
       <Routes>
         <Route path="/" element={<Landing user={user} />} />
         <Route
---
+++ src/components/FakeComponent/FakeComponent.jsx
@@ -0,0 +1,5 @@
+const FakeComponent = () => {
+  return ( <h1>Fake Component</h1> );
+}
+ 
+export default FakeComponent;
