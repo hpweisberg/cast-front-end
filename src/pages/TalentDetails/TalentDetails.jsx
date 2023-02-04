09cfa1eba08613a3caa37e0f7100b5616cd8a11b
branhickman <branhickman@gmail.com>
2023-02-04 12:47:59 -0700

    Setup Education component and nest in TalentDetails page

--- src/pages/TalentDetails/TalentDetails.jsx
+++ src/pages/TalentDetails/TalentDetails.jsx
@@ -1,11 +1,13 @@
 // components
 import Experience from "../../components/Experience/Experience";
+import Education from "../../components/Education/Education";
 
 const TalentDetails = () => {
   return ( 
     <>
       <h1>Talent Details Component</h1> 
       <Experience />
+      <Education />
     </>
   );
 }
