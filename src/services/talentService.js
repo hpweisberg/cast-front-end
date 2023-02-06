1ca107ac2cdfb164775522241eeaa63abc1d34a6
hpweisberg <hpweisberg@gmail.com>
2023-02-06 10:21:06 -0800

    complete talentService file

--- src/services/talentService.js
+++ src/services/talentService.js
@@ -13,3 +13,52 @@ const index = async () => {
   }
 }
 
+const show = async(id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/$id`, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` }
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const update = async (talentData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${talentData._id}`, {
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(talentData)
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//? Double check this is correct. Should delete a specific talent's experince, education, etc.
+const deleteTalentAttribute = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      }
+    })
+    return res.json()
+  } catch (error) {
+  console.log(error)
+  }
+}
+
+
+export {
+  index,
+  show,
+  update,
+  deleteTalentAttribute
+}
