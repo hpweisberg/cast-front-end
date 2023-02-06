652aec34cb4a9ebeb2d527c4a713d22dd8c791de
hpweisberg <hpweisberg@gmail.com>
2023-02-06 11:05:08 -0800

    add useState and useEffect for talent search

--- src/pages/ListDetails/ListDetails.jsx
+++ src/pages/ListDetails/ListDetails.jsx
@@ -2,7 +2,7 @@ const ListDetails = (props) => {
   console.log('List Detials Props:', props)
   return ( 
     <h1>List Details Component</h1> 
-      // {props.}
+      
   );
 }
 
--- src/pages/TalentSearch/TalentSearch.jsx
+++ src/pages/TalentSearch/TalentSearch.jsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
-// import * as ta
+import * as talentService from '../../services/talentService'
+
 
 const TalentSearch = () => {
-  // const [talentSearch, setTalentSearch] = useState([])
+  const [talentSearch, setTalentSearch] = useState([])
+
+  useEffect(() => {
+    const fetchTalent = async () => {
+      const talentData = await talentService.index()
+      setTalentSearch(talentData)
+    }
+    fetchTalent()
+  }, [])
   
   return ( 
     <h1>Talent Search Component</h1>
