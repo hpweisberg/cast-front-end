940e1b9cc984bfef4da668dc13a7c314338939f3
hpweisberg <hpweisberg@gmail.com>
2023-02-06 20:36:02 -0800

    adjust styling for menu and complete toggle function

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,29 +11,39 @@ const NavBar = ({ user, handleLogout }) => {
   }
 
   return (
-    <nav>
-      <div className={styles.menuIcon} onClick={toggle}>
-        <Icon name='Menu' />
+    <>
+      <div>
+
+
+        <div className={styles.menuIcon} onClick={toggle}>
+          <Icon name='Menu' />
+        </div>
+        {isOpen && (
+
+          <nav>
+
+          {user ?
+            <ul>
+              <li>Welcome, {user.name}</li>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/talent'>Talent Search</Link></li>
+              <li><Link to='lists'>Lists</Link></li>
+              <li><Link to='/profile'>Profile</Link></li>
+              {/* Items below here will be removed in final app */}
+              <li><Link to='/profiles'>Profiles</Link></li>
+              <li><Link to='/change-password'>Change Password</Link></li>
+              <li><Link to='' onClick={handleLogout}>LOG OUT</Link></li>
+            </ul>
+            :
+            <ul>
+              <li><Link to='/login'>Log In</Link></li>
+              <li><Link to='/signup'>Sign Up</Link></li>
+            </ul>
+          }
+        </nav>
+              )}
       </div>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/talent'>Talent Search</Link></li>
-          <li><Link to='lists'>Lists</Link></li>
-          <li><Link to='/profile'>Profile</Link></li>
-          {/* Items below here will be removed in final app */}
-          <li><Link to='/profiles'>Profiles</Link></li>
-          <li><Link to='/change-password'>Change Password</Link></li>
-          <li><Link to='' onClick={handleLogout}>LOG OUT</Link></li>
-        </ul>
-        :
-        <ul>
-          <li><Link to='/login'>Log In</Link></li>
-          <li><Link to='/signup'>Sign Up</Link></li>
-        </ul>
-      }
-    </nav>
+    </>
   )
 }
 
