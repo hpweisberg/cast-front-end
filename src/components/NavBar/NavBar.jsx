d5b6cf9de5c8406003260fd1943cfe06a57eab05
hpweisberg <hpweisberg@gmail.com>
2023-02-06 20:27:56 -0800

    add pop up menu funciton and button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,20 @@
+import styles from './NavBar.module.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import Icon from '../Icon/Icon'
 
 const NavBar = ({ user, handleLogout }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <nav>
+      <div className={styles.menuIcon} onClick={toggle}>
+        <Icon name='Menu' />
+      </div>
       {user ?
         <ul>
           <li>Welcome, {user.name}</li>
@@ -15,7 +27,7 @@ const NavBar = ({ user, handleLogout }) => {
           <li><Link to='/change-password'>Change Password</Link></li>
           <li><Link to='' onClick={handleLogout}>LOG OUT</Link></li>
         </ul>
-      :
+        :
         <ul>
           <li><Link to='/login'>Log In</Link></li>
           <li><Link to='/signup'>Sign Up</Link></li>
