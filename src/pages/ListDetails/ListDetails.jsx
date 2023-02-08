1868286cb660499d7c3b165a31ac974286ce8996
KevinRoost <kroost92@gmail.com>
2023-02-08 11:47:14 -0500

    debug list index and list details

--- src/pages/ListDetails/ListDetails.jsx
+++ src/pages/ListDetails/ListDetails.jsx
@@ -7,24 +7,17 @@ import * as cdService from '../../services/cdService'
 
 const ListDetails = (props) => {
   const location = useLocation()
-  const { list, profile } = location.state
-  // const [list, setList] = useState({})
-  // console.log(props);
-  // useEffect(() => {
-  //   const fetchList = async () => {
-  //     const list = await cdService.showList(profile.cdAccount, props.key)
-  //     setList(list)
-  //     console.log('details',props);
-  //   }
-  //   fetchList()
-  // }, [props.profile.cdAccount, props.key])
+  const { shownList } = location.state
+  console.log(shownList);
+
 
   // console.log('List Details Props:', profile)
+
   return ( 
     <>
       <h1>List Details Component</h1> 
-      <h3>{list.titleOfList}</h3>
-      {list.talent.map(talent => (
+      <h3>{shownList.titleOfList}</h3>
+      {shownList.talent.map(talent => (
         <TalentCard key={talent} profile={talent.profile} talent={talent._id}/>
       ))}
     </>
