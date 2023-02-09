74ec8054f9dc9ca29aa3cb42dd279887e46697e1
branhickman <branhickman@gmail.com>
2023-02-08 18:08:20 -0700

    revert changes

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,27 +1,19 @@
 const Education = (props) => {
+  const education = props.education
+
   return ( 
     <>
-      <h1>Education:</h1> 
-  
-      {props.education?.map((edu, idx) => (
-        <div key={idx}>
-          <div className="eduName">
-          <p>{edu.institution}</p>
-          </div>
-          <div className="eduMid">
-          <p>{edu.degree}</p>
-          <p>{edu.major}</p>
-          <p>{edu.graduated}</p>
-          <p>Graduated: {edu.graduated ? "Yes" : "No"}</p>
-          </div>
-          <form onSubmit={()=> props.handleDeleteEducation(props.talentId, edu._id)}>
+      <h1>Education Component</h1> 
+      <p>Institution: {education.institution}</p>
+      <p>Degree: {education.degree}</p>
+      <p>Major: {education.major}</p>
+      <p>Graduated: {education.graduated ? "Yes" : "No"}</p>
+      <p>Year: {education.year}</p>
+      <form onSubmit={()=> props.handleDeleteEducation(props.talentId, education._id)}>
         <button type='submit'>Delete</button>
       </form>
-        </div>
-      ))}
-      </>
-  
-  )
+    </>
+  );
 }
-
+ 
 export default Education;
