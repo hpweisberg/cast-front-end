be5f37fe348ee1fb06cda6f18e236ac92508d00e
hpweisberg <hpweisberg@gmail.com>
2023-02-09 13:15:09 -0800

    add styling to expereince

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,17 +1,20 @@
 import { useParams } from "react-router-dom";
+import styles from './Experince.module.css'
 
 const Experience = (props) => {
   const experience = props.experience
 
   return ( 
     <>
-      <h1>Experience Component</h1>
-      <p>Production Title: {experience.productionTitle}</p>
-      <p>Role: {experience.role}</p>
-      <p>Type: {experience.type}</p>
-      <p>Producing Entity: {experience.producingEntity}</p>
-      <p>Director: {experience.director}</p>
-      <p>Year: {experience.year}</p>
+    <div className={styles.container}>
+      <h1>Experience</h1>
+      <p><span className={styles.smallText}>Production Title: </span>{experience.productionTitle}</p>
+      <p><span className={styles.smallText}>Role: </span>{experience.role}</p>
+      <p><span className={styles.smallText}>Type: </span>{experience.type}</p>
+      <p><span className={styles.smallText}>Producing Entity: </span>{experience.producingEntity}</p>
+      <p><span className={styles.smallText}>Director: </span>{experience.director}</p>
+      <p><span className={styles.smallText}>Year: </span>{experience.year}</p>
+    </div>
     </>
   );
 }
