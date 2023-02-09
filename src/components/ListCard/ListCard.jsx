1eee3cc49a821a19274ec4dca1dab42ad56ec776
KevinRoost <kroost92@gmail.com>
2023-02-09 11:20:48 -0500

    style list cards

--- src/components/ListCard/ListCard.jsx
+++ src/components/ListCard/ListCard.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
 import { useState, useEffect } from "react";
 import * as cdService from '../../services/cdService'
+import Icon from '../../components/Icon/Icon'
+
+import './ListCard.css'
+
 // import { useParams } from "react-router-dom"
 // import Icon from '../../components/Icon/Icon'
 // import TalentSearch from "../../pages/TalentSearch/TalentSearch";
@@ -8,46 +12,21 @@ import * as cdService from '../../services/cdService'
 // import styles from './ListCard.module.css'
 
 const ListCard = ({ list, profile, handleDeleteList }) => {
-console.log(list);
 
-// useEffect(() => {
-//   const fetchList = async () => {
-//     const target = await cdService.showList(profile.cdAccount, list._id)
-//     setShownList(target)
-//   }
-//   fetchList()
-// }, [profile.cdAccount, list._id])
 return (
-  <>
-    <button onClick={()=>{handleDeleteList((list?.id))}}>x</button>
-    <Link to={`/cd/${profile.cdAccount._id}/lists/${list._id}`}>
+
+    <Link className='card'to={`/cd/${profile.cdAccount._id}/lists/${list._id}`}>
+      <button onClick={()=>{handleDeleteList((list?.id))}}>x</button>
       <h4>
         {list.titleOfList}
       </h4>
-      <p>{list.talent?.length}</p>
-
+      <div className='people'>
+        <p>{list.talent?.length}</p>
+        <Icon name='People' />
+      </div>  
     </Link>
-</>
+
 )
-
-  // return (
-  //   <div className={styles.center}>
-  //     <Link to={`/profiles`} className={styles.link}>
-  //       <section className={styles.listCardContainer}>
-  //         <img src={props.profile.photo} alt="user profile pic" className={styles.photo}></img>
-  //         <div className={styles.overflow}></div>
-  //         <div className={styles.glanceInfo}>
-
-  //           <h3>{props.profile.name}</h3>
-  //           <p className={styles.pronouns}>{props.profile.pronouns}</p>
-  //           <p className={styles.union}>{props.profile.talentAccount.unionStatus}</p>
-  //           <p className={styles.location}>{props.profile.location}</p>
-  //           <p className={styles.reelsIcon}><Icon name='Reels' className={styles.reelsIcon} /> {props.profile.talentAccount?.reel}</p>
-  //         </div>
-  //       </section>
-  //     </Link>
-  //   </div>
-  // )
 }
 
 export default ListCard;
