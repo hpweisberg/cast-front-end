11c87f77bbe489ea78ae9178dec3eed8a41d1a57
hpweisberg <hpweisberg@gmail.com>
2023-02-09 13:46:18 -0800

    add training styling

--- src/components/Training/Training.jsx
+++ src/components/Training/Training.jsx
@@ -1,18 +1,27 @@
-
+import styles from './Training.module.css'
 
 const Training = (props) => {
   const training = props.training
-  
 
 
-  return ( 
+
+  return (
     <>
-      <h1>Training Component</h1> 
-      <p>Instituion: {training.institution}</p>
-      <p>Focus: {training.focus}</p>
-      <p>Teacher: {training.teacher}</p>
+      <h1>Training</h1>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <p><span className={styles.smallText}>Instituion: </span></p>
+          <p><span className={styles.smallText}>Focus: </span></p>
+          <p><span className={styles.smallText}>Teacher:  </span></p>
+        </div>
+        <div className={styles.right}>
+          <p>{training.institution}</p>
+          <p>{training.focus}</p>
+          <p>{training.teacher}</p>
+        </div>
+      </div>
     </>
   );
 }
- 
+
 export default Training;
