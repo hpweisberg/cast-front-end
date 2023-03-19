08bc4b81c2714daa66a61c7ebde206d3ec528a47
KevinRoost <kroost92@gmail.com>
2023-03-19 10:59:35 -0400

    apply edit profile styling to create profile page

--- src/pages/Profile/CreateProfile.jsx
+++ src/pages/Profile/CreateProfile.jsx
@@ -1,3 +1,4 @@
+import styles from './EditProfile.module.css'
 import { useState } from "react"
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
@@ -78,164 +79,170 @@ const CreateProfile = (props) => {
 
 
   return (
-    <>
-      <h2>Edit Profile Details</h2>
-      <form>
-        <label htmlFor="pronouns-input">Pronouns</label>
-        <select
-          name="pronouns"
-          id="pronouns-input"
-          onChange={handleProfileChange}
-          value={form.pronouns}
-        >
-          <option value="Please Select">Please Select</option>
-          <option value="He/Him/His">He/Him/His</option>
-          <option value="She/Her/Hers">She/Her/Hers</option>
-          <option value="They/Them/Theirs">They/Them/Theirs</option>
-        </select>
-        <label htmlFor="location-input">Location</label>
-        <input
-          type="text"
-          name="location"
-          id="location-input"
-          onChange={handleProfileChange}
-          value={form.location}
-        />
-        <label htmlFor="phone-number-input">Phone Number</label>
-        <input
-          type="tel"
-          name="phoneNumber"
-          id="phone-number-input"
-          onChange={handleProfileChange}
-          value={form.phoneNumber}
-        />
-        <label htmlFor="website-input">Website</label>
-        <input
-          type="text"
-          name="website"
-          id="website-input"
-          onChange={handleProfileChange}
-          value={form.website}
-        />
-      </form>
+    <div class="edit-profile">
+      <div className="edit-profile-details">
+        <h2>Edit Profile Details</h2>
+        <form className={styles.column} >
+          <label htmlFor="pronouns-input">Pronouns</label>
+          <select
+            name="pronouns"
+            id="pronouns-input"
+            onChange={handleProfileChange}
+            value={form.pronouns}
+          >
+            <option value="Please Select">Please Select</option>
+            <option value="He/Him/His">He/Him/His</option>
+            <option value="She/Her/Hers">She/Her/Hers</option>
+            <option value="They/Them/Theirs">They/Them/Theirs</option>
+          </select>
+          <label htmlFor="location-input">Location</label>
+          <input
+            type="text"
+            name="location"
+            id="location-input"
+            onChange={handleProfileChange}
+            value={form.location}
+          />
+          <label htmlFor="phone-number-input">Phone Number</label>
+          <input
+            type="tel"
+            name="phoneNumber"
+            id="phone-number-input"
+            onChange={handleProfileChange}
+            value={form.phoneNumber}
+          />
+          <label htmlFor="website-input">Website</label>
+          <input
+            type="text"
+            name="website"
+            id="website-input"
+            onChange={handleProfileChange}
+            value={form.website}
+          />
+        </form>
+      </div>
       {(render === false) ?
         <>
-          <h2>Talent Account Details</h2>
-          <form onSubmit={handleTalentSubmit}>
-            <label htmlFor="union-status-input">Union Status</label>
-            <select
-              name="unionStatus"
-              id="union-status-input"
-              value={talentForm.unionStatus}
-              onChange={handleTalentChange}
-            >
-              <option value="Please Select">Please Select</option>
-              <option value="SAG">SAG</option>
-              <option value="AEA">AEA</option>
-              <option value="Not Affiliated">Not Affiliated</option>
-            </select>
-            <label htmlFor="hair-input">Hair</label>
-            <select
-              name="hair"
-              id="hair-input"
-              value={talentForm.hair}
-              onChange={handleTalentChange}
-            >
-              <option value="Please Select">Please Select</option>
-              <option value="Black">Black</option>
-              <option value="Brown">Brown</option>
-              <option value="Blonde">Blonde</option>
-              <option value="Red">Red</option>
-              <option value="Grey">Grey</option>
-              <option value="Other">Other</option>
-            </select>
-            <label htmlFor="eyes-input">Eyes</label>
-            <select
-              name="eyes"
-              id="eyes-input"
-              value={talentForm.eyes}
-              onChange={handleTalentChange}
-            >
-              <option value="Please Select">Please Select</option>
-              <option value="Brown">Brown</option>
-              <option value="Green">Green</option>
-              <option value="Blue">Blue</option>
-              <option value="Other">Other</option>
-            </select>
-            <label htmlFor="height">Height (in inches)</label>
-            <input
-              id='height'
-              type="number"
-              name="height"
-              value={talentForm.height}
-              onChange={handleTalentChange}
-            />
-            <label htmlFor="weight-input">Weight</label>
-            <input
-              id='weight-input'
-              type="number"
-              value={talentForm.weight}
-              name="weight"
-              onChange={handleTalentChange}
-            />
-            <label htmlFor="about-input">About</label>
-            <textarea
-              name="about"
-              id="about-input"
-              cols="30"
-              rows="10"
-              value={talentForm.about}
-              onChange={handleTalentChange}
-            >
-            </textarea>
-            <label htmlFor="skills-input">Skills</label>
-            <select
-              name="skills"
-              id="skills-input"
-              value={talentForm.category}
-              onChange={handleTalentChange}
-            >
-              <option value="Please Select">Please Select</option>
-              <option value="Brown">Brown</option>
-            </select>
-            <label htmlFor="trades-input">Trades</label>
-            <select
-              name="trades"
-              id="trades-input"
-              value={talentForm.category}
-              onChange={handleTalentChange}
-            >
-              <option value="Please Select">Please Select</option>
-              <option value="Brown">Brown</option>
-            </select>
-            <label htmlFor="reel-input">Reel Link</label>
-            <input
-              type="text"
-              id="reel-input"
-              name="reelLink"
-              value={talentForm.reelLink}
-              onChange={handleTalentChange}
-            />
-            <button type="submit">Save</button>
-          </form>
+          <div className="edit-profile-details">
+            <h2>Talent Account Details</h2>
+            <form className={styles.column} onSubmit={handleTalentSubmit}>
+              <label htmlFor="union-status-input">Union Status</label>
+              <select
+                name="unionStatus"
+                id="union-status-input"
+                value={talentForm.unionStatus}
+                onChange={handleTalentChange}
+              >
+                <option value="Please Select">Please Select</option>
+                <option value="SAG">SAG</option>
+                <option value="AEA">AEA</option>
+                <option value="Not Affiliated">Not Affiliated</option>
+              </select>
+              <label htmlFor="hair-input">Hair</label>
+              <select
+                name="hair"
+                id="hair-input"
+                value={talentForm.hair}
+                onChange={handleTalentChange}
+              >
+                <option value="Please Select">Please Select</option>
+                <option value="Black">Black</option>
+                <option value="Brown">Brown</option>
+                <option value="Blonde">Blonde</option>
+                <option value="Red">Red</option>
+                <option value="Grey">Grey</option>
+                <option value="Other">Other</option>
+              </select>
+              <label htmlFor="eyes-input">Eyes</label>
+              <select
+                name="eyes"
+                id="eyes-input"
+                value={talentForm.eyes}
+                onChange={handleTalentChange}
+              >
+                <option value="Please Select">Please Select</option>
+                <option value="Brown">Brown</option>
+                <option value="Green">Green</option>
+                <option value="Blue">Blue</option>
+                <option value="Other">Other</option>
+              </select>
+              <label htmlFor="height">Height (in inches)</label>
+              <input
+                id='height'
+                type="number"
+                name="height"
+                value={talentForm.height}
+                onChange={handleTalentChange}
+              />
+              <label htmlFor="weight-input">Weight</label>
+              <input
+                id='weight-input'
+                type="number"
+                value={talentForm.weight}
+                name="weight"
+                onChange={handleTalentChange}
+              />
+              <label htmlFor="about-input">About</label>
+              <textarea
+                name="about"
+                id="about-input"
+                cols="30"
+                rows="10"
+                value={talentForm.about}
+                onChange={handleTalentChange}
+              >
+              </textarea>
+              <label htmlFor="skills-input">Skills</label>
+              <select
+                name="skills"
+                id="skills-input"
+                value={talentForm.category}
+                onChange={handleTalentChange}
+              >
+                <option value="Please Select">Please Select</option>
+                <option value="Brown">Brown</option>
+              </select>
+              <label htmlFor="trades-input">Trades</label>
+              <select
+                name="trades"
+                id="trades-input"
+                value={talentForm.category}
+                onChange={handleTalentChange}
+              >
+                <option value="Please Select">Please Select</option>
+                <option value="Brown">Brown</option>
+              </select>
+              <label htmlFor="reel-input">Reel Link</label>
+              <input
+                type="text"
+                id="reel-input"
+                name="reelLink"
+                value={talentForm.reelLink}
+                onChange={handleTalentChange}
+              />
+              <button type="submit">Save</button>
+            </form>
+          </div>
         </>
         :
         <>
           <h2>CD Account</h2>
-          <form onSubmit={handleCDSubmit}>
-            <label htmlFor="company-input">Company</label>
-            <input
-              type="text"
-              name="company"
-              id="company-input"
-              value={CDForm.company}
-              onChange={handleCDChange}
-            />
-            <button type="submit">Save</button>
-          </form>
+          <div>
+            <form onSubmit={handleCDSubmit}>
+              <label htmlFor="company-input">Company</label>
+              <input
+                type="text"
+                name="company"
+                id="company-input"
+                value={CDForm.company}
+                onChange={handleCDChange}
+              />
+              <button type="submit">Save</button>
+            </form>
+          </div>
         </>
       }
-    </>
+    </div>
   )
 }
 
